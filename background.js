cbb8633c40424491c9e270a721da7242ad7ccf9e
Louis <louis.billaud@cbien.com>
2017-12-13 15:54:17 +0100

    Init

---
+++ background.js
@@ -0,0 +1,7 @@
+chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
+  var url = tab.url;
+  var substring = new Array('beatport', 'soundcloud', 'youtube');
+  for (i = 0; i < 3; ++i)
+    if (url.indexOf(substring[i]) !== -1)
+      chrome.pageAction.show(tabId);
+});
