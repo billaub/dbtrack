e760e68f3a028b407bbaa6b62dd333f9bc3f5474
Louis <louis.billaud@cbien.com>
2017-12-29 09:00:15 +0100

    dbtrack v1.1

--- save.js
+++ save.js
@@ -1,23 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var save_button = document.getElementById("toto");
-  console.log(save_button);
-  save_button.addEventListener('click', function() {
-    var url = ''
-      chrome.tabs.getSelected(null, function (tab) {
+document.addEventListener('DOMContentLoaded', function () {
+    let save_button = document.getElementById("toto");
+    console.log(save_button);
+    let url = '';
+    chrome.tabs.getSelected(null, function (tab) {
         url = tab.url;
-        insert = {
-          ['track_' + url]: url
-        }
-        chrome.storage.sync.set(insert, function () {console.log('done');});
-        chrome.storage.sync.get(null, function (obj) {
-          console.log(obj);
+        save_button.addEventListener('click', function () {
+            insert = {
+                ['track_' + url]: url
+            };
+            chrome.storage.sync.set(insert, function () {
+                chrome.notifications.create("track_saved", {
+                    title: "dbtrack",
+                    iconUrl: "icon.png",
+                    type: "basic",
+                    message: decodeURIComponent(escape("Url enregistrée")),
+                }, () => {
+                });
+            });
+            chrome.storage.sync.get(null, function (obj) {
+                console.log(obj);
+            });
         });
-      });
-  });
+    });
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-  var open_button = document.getElementById("open_tracks");
+  let open_button = document.getElementById("open_tracks");
   console.log(open_button);
   open_button.addEventListener('click', function() {
     chrome.tabs.create({url:"gestion.html"});
