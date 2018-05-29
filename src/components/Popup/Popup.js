503b906ba872d8b1f5cac494bbc284dc7980be88
Zarbuz <nicolas.perrier2@numericable.fr>
2018-05-29 20:06:45 +0200

    [Popup] Can save the url of the current tab

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,15 +1,40 @@
+/*global chrome*/
 import React, { Component } from 'react';
 import './Popup.css';
 
 class Popup extends Component {
+
+  constructor(props) {
+    super(props);
+    this.saveUrl = this.saveUrl.bind(this);
+    this.openTab = this.openTab.bind(this);
+  }
+
+  saveUrl() {
+    chrome.tabs.getSelected(null, function (tab) {
+      let insert = {
+        ['track_' + tab.url]: tab.url
+      };
+      chrome.storage.sync.set(insert, function () {
+        if (chrome.runtime.error) {
+          console.log("Runtime error.");
+        }
+      });
+    });
+  }
+
+  openTab() {
+
+  }
+
   render() {
     return (
       <div>
         <div className="first" id="container">
-          <button className="save_button" id="toto">Sauvegarder l'url</button>
+          <button className="save-button button-green" onClick={this.saveUrl}>Sauvegarder l'url</button>
         </div>
         <div className="second">
-          <button className="button_blue" id="open_tracks">Voir les urls sauvegardées</button>
+          <button className="save-button button-red" onClick={this.openTab}>Voir les urls sauvegardées</button>
         </div>
       </div>
     );
