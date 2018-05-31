7de3bb41ce392e24ce89b14b02d8b801782a4625
Zarbuz <nicolas.perrier2@numericable.fr>
2018-05-31 16:35:33 +0200

    [Popup] Disabled the save button if we aren't in a valid link

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -6,39 +6,43 @@ class Popup extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      saveButtonActive: true
+    }
+
     this.saveUrl = this.saveUrl.bind(this);
     this.openTab = this.openTab.bind(this);
     this.sendPopup = this.sendPopup.bind(this);
     this.checkUrl = this.checkUrl.bind(this);
   }
 
-  componentDidMount() {
-      chrome.tabs.getSelected(null, function (tab) {
-          let url = tab.url;
-          if (!this.checkUrl(url)) {
-              //  TODO : Finish Me (Disable save button)
-          }
-      });
+  componentWillMount() {
+    let p = this;
+    chrome.tabs.getSelected(null, function (tab) {
+      let url = tab.url;
+      p.setState({ saveButtonActive: p.checkUrl(url) });
+    });
   }
 
   checkUrl(url) {
-      console.log("checking url: " + url);
-      let regex = [
-          ".*youtube.com/watch.*",
-          ".*beatport.com/.*",
-          ".*soundcloud.com/.*"
-      ];
-      let re = new RegExp(regex.join("|"), "gi");
-      console.log("url matches: " + re.test(url));
-      return re.test(url)
+    console.log("checking url: " + url);
+    let regex = [
+      ".*youtube.com/watch.*",
+      ".*beatport.com/.*",
+      ".*soundcloud.com/.*"
+    ];
+    let re = new RegExp(regex.join("|"), "gi");
+    let value = re.test(url);
+    console.log("url matches: " + value);
+    return value;
   }
 
   sendPopup(msg, icon) {
     chrome.notifications.create("track_saved", {
-        title: "dbtrack",
-        iconUrl: icon,
-        type: "basic",
-        message: msg,
+      title: "dbtrack",
+      iconUrl: icon,
+      type: "basic",
+      message: msg,
     }, () => {
     });
   }
@@ -66,10 +70,15 @@ class Popup extends Component {
   }
 
   render() {
+
     return (
       <div>
         <div className="first" id="container">
-          <button className="save-button button-green" onClick={this.saveUrl}>Sauvegarder l'url</button>
+          {this.state.saveButtonActive ? (
+            <button className="save-button button-green" onClick={this.saveUrl}>Sauvegarder l'url</button>
+          ) : (
+              <button className="save-button button-grey">Sauvegarder l'url</button>
+            )}
         </div>
         <div className="second">
           <button className="save-button button-blue" onClick={this.openTab}>Voir les urls sauvegardées</button>
