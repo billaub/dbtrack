c8e7ab21cbc65bba88bce1df3cc7d2016f517c6b
Zarbuz <nicolas.perrier2@numericable.fr>
2018-05-31 11:08:21 +0200

    [Header] Added the searchbar

---
+++ src/components/Header/Searchbar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+//import './Searchbar.css';
+
+class Searchbar extends Component {
+
+    render() {
+        return (
+            <form className="navbar-form navbar-left input-s-lg m-t m-l-n-xs hidden-xs" role="search">
+                <div className="form-group">
+                    <div className="input-group">
+                        <span className="input-group-btn">
+                            <button type="submit" className="btn btn-sm bg-white btn-icon rounded">
+                                <i className="fa fa-search"></i>
+                            </button>
+                        </span>
+                        <input type="text" className="form-control input-sm no-border rounded" placeholder="Chercher le titre d'une musique, d'un album ..." />
+                    </div>
+                </div>
+            </form>
+        );
+    }
+}
+
+export default Searchbar;
