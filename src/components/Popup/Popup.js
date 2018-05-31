e232b8ca670958c572c831d55b210050dadd237d
Louis <louis.billaud@epita.fr>
2018-05-31 14:33:30 +0200

    Updated background.js

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -8,22 +8,39 @@ class Popup extends Component {
     super(props);
     this.saveUrl = this.saveUrl.bind(this);
     this.openTab = this.openTab.bind(this);
+    this.sendPopup = this.sendPopup.bind(this);
+  }
+
+  sendPopup(msg) {
+    console.log("toto");
+    chrome.notifications.create("track_saved", {
+        title: "dbtrack",
+        iconUrl: "icon.png",
+        type: "basic",
+        message: msg,
+    }, () => {
+    });
   }
 
   saveUrl() {
+    let p = this;
     chrome.tabs.getSelected(null, function (tab) {
       let insert = {
         ['track_' + tab.url]: tab.url
       };
-      chrome.storage.sync.set(insert, function () {
+      chrome.storage.sync.set(insert, () => {
         if (chrome.runtime.error) {
           console.log("Runtime error.");
+        }
+        else {
+          p.sendPopup("Url enregistrée");
         }
       });
     });
   }
 
   openTab() {
+    console.log("opening tab");
     chrome.tabs.create({ url: "dashboard.html" });
   }
 
@@ -34,7 +51,7 @@ class Popup extends Component {
           <button className="save-button button-green" onClick={this.saveUrl}>Sauvegarder l'url</button>
         </div>
         <div className="second">
-          <button className="save-button button-red" onClick={this.openTab}>Voir les urls sauvegardées</button>
+          <button className="save-button button-blue" onClick={this.openTab}>Voir les urls sauvegardées</button>
         </div>
       </div>
     );
