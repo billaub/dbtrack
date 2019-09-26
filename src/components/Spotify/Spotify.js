09f8efed55c0631554b197de43c1c6bacec70044
Louis Billaud <louis.billaud@epita.fr>
2019-09-26 15:21:55 +0200

    beginning the work on spotify

---
+++ src/components/Spotify/Spotify.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import GridLink from '../Grid/GridLink';
+
+class Content extends Component {
+
+    render() {
+        return (
+            <section id="content">
+                toto
+            </section>
+        );
+    }
+}
+
+export default Content;
